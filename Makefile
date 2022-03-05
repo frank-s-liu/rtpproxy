366f61fb4cb4eb92eb9bca3223020916b060f4a7
frank-s-liu <lyyslsw1230@163.com>
2022-03-05 07:52:41 +0000

    test

---
+++ Makefile
@@ -0,0 +1,100 @@
+ODIR := ./objs/
+
+Cxx_SRCS := main.cpp
+Cxx_ODIR := ./objs/cxxobjs/
+Cxx_OBJS := $(addprefix $(Cxx_ODIR),$(Cxx_SRCS:.cpp=.o))  
+Cxx_DEPS := $(addprefix $(Cxx_ODIR),$(Cxx_SRCS:.cpp=.d)) 
+
+
+C_SRCS := 
+C_ODIR := ./objs/cobjs/
+C_OBJS := $(addprefix $(C_ODIR),$(C_SRCS:.c=.o))  
+C_DEPS := $(addprefix $(C_ODIR),$(C_SRCS:.c=.d)) 
+
+TARGET=rtpproxy
+
+MAJ ?= 2
+MIN ?= 0
+export MAJ MIN
+
+INCLUDE_DIR := -I./export/logExport -I ./export/utilExport -I ./export/mempoolExport -I ./tinyxml/export -I ./export/timerEventExport
+CXXFLAGS := -g -fPIC -Wall -Werror -fno-omit-frame-pointer -fstack-protector  $(INCLUDE_DIR)
+CFLAGS := -g -fPIC -Wall -Werror -fno-omit-frame-pointer -fstack-protector $(INCLUDE_DIR)
+
+LDFLAGS :=  -llog -lmemory -lutilself -ltinyxml -pthread -lssl -lcrypto  -ltimerEvent  -lrtp  -L./mempool/mempoolobjs -L./log/logobjs -L./tinyxml/libs -L./linklibs -L./timerEvent/timerEventObjs  -L ./rtp/rtp_objs -Wl,-rpath=../libs -Wl,-rpath-link=../libs
+
+$(TARGET): ALL
+	cd ./mempool; make || exit "$$"; cd -
+	cd ./log; make || exit "$$"; cd -
+	cd ./util; make || exit "$$"; cd -
+	cd ./timerEvent; make || exit "$$"; cd -
+	cd ./rtp; make || exit "$$"; cd -
+	test -d ./bin || mkdir ./bin
+	g++ $(LDFLAGS) $(Cxx_OBJS) $(C_OBJS)     -o ./bin/$@
+
+ALL: $(Cxx_OBJS) $(C_OBJS)
+
+
+$(Cxx_ODIR)%o: %cpp
+	test -d $(Cxx_ODIR) || mkdir -p $(Cxx_ODIR)
+	g++ -c $(CXXFLAGS) $< -o $@
+
+
+
+$(C_ODIR)%o: %c
+	test -d $(C_ODIR) || mkdir -p $(C_ODIR)
+	gcc -c $(CFLAGS) $< -o $@
+
+#using '-', indicate that if make don't find the depentfile, don't care the errors,
+#at begining, still don't produce the dependent file
+ifneq ($(MAKECMDGOALS),clean) 
+include $(Cxx_DEPS)
+include $(C_DEPS)
+endif
+
+$(Cxx_ODIR)%d:%cpp
+	test -d $(Cxx_ODIR) || mkdir -p $(Cxx_ODIR)
+	@set -e; rm -f $@;
+	g++ -MM $(CXXFLAGS) $< > $@.123456;
+	sed 's,\($*\)\.o[ :]*, $(Cxx_ODIR)\1.o $@ : ,g' < $@.123456 > $@;
+	rm -f $@.123456
+
+
+$(C_ODIR)%.d:%.c
+	test -d $(C_ODIR) || mkdir -p $(C_ODIR)
+	@set -e; rm -f $@;
+	gcc -MM $(CFLAGS) $< > $@.123456;
+	sed 's,\($*\)\.o[ :]*, $(C_ODIR)\1.o $@ : ,g' < $@.123456 > $@;
+	rm -f $@.123456
+
+
+
+.PHONY: install
+install:
+	rm -rf ./libs
+	install -d ./libs
+	install ./mempool/mempoolobjs/libmemory.so* ./libs
+	install ./util/utilobjs/libutilself.so* ./libs
+	install ./log/logobjs/liblog.so* ./libs
+	install ./tinyxml/libs/libtinyxml.so*  ./libs
+	install ./timerEvent/timerEventObjs/lib*.so* ./libs
+	install ./linklibs/lib*.so*  ./libs
+	mkdir -p rtpproxy
+	cp -rf bin libs rtpproxy
+	cp -f conf/*  rtpproxy/conf/
+	tar -zcvf rtpproxy.tar.gz rtpproxy
+	rm -rf rtpproxy
+
+.PHONY: clean
+clean:  
+	rm -rf $(ODIR)
+	cd ./mempool;make clean;cd -
+	cd ./util;make clean;cd -
+	cd ./log;make clean; cd -
+	cd ./timerEvent;make clean; cd -
+	cd ./rtp;make clean; cd -
+	rm -rf ./bin/$(TARGET)
+	rm -rf ./bin/client
+	rm -rf ./libs
+	rm -f $(TARGET)
+	rm -f *.tar.gz
---
+++ main.cpp
@@ -0,0 +1,319 @@
+#include "log.h"
+#include "tinyxml.h"
+#include "task.h"
+
+#include <assert.h>
+#include <fcntl.h>
+#include <sys/resource.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <string.h>
+#include <getopt.h>
+
+//DEBUG
+#include <stdio.h>
+
+typedef struct logmodule
+{
+    char*               module_name;
+    int                 level;
+    struct logmodule *  next;
+}LogModule_l;
+
+typedef struct logconfig
+{
+    char*        logpath;
+    LogModule_l* modules;
+}LogConfigure;
+
+void daemonize()
+{
+    int fd0, fd1, fd2;
+    pid_t pid;
+    struct rlimit rl;
+    struct sigaction sa;
+
+    // Clear file creation mask.
+    umask(0);
+
+    // Get maximum number of file descriptors. 
+    if (getrlimit(RLIMIT_NOFILE, &rl) < 0)
+    {
+        exit(1);
+    }
+    // Become a session leader to lose controlling TTY. 
+    if ((pid = fork()) < 0)
+    {
+        exit(1);
+    }
+    else if (pid != 0)
+    {
+        // parent exit
+        exit(0);
+    }
+    setsid();
+
+    // Ensure future opens won't allocate controlling TTYs. 
+    sa.sa_handler = SIG_IGN;
+    sigemptyset(&sa.sa_mask);
+    sa.sa_flags = 0;
+    if (sigaction(SIGHUP, &sa, NULL) < 0)
+    {
+        exit(1);
+    }
+
+    if ((pid = fork()) < 0)
+    {
+        exit(1);
+    }
+    else if (pid != 0)
+    {
+        exit(0);
+    }
+
+    close(0);
+    close(1);
+    close(2);
+    // Attach file descriptors 0, 1, and 2 to /dev/null. 
+    fd0 = open("/dev/null", O_RDWR); // fd0 == 0
+    fd1 = dup(0);                    // fd1 == 1
+    fd2 = dup(0);                    // fd2 == 2
+    (void)fd0;
+    (void)fd1;
+    (void)fd2;
+}
+
+int parseXMLconfiguration(const char* path_name, LogConfigure* logconfigs)
+{
+    TiXmlDocument xml_conf;
+    if(!xml_conf.LoadFile(path_name))
+    {
+        assert(0);
+        return -1;
+    }
+
+    // root poit
+    TiXmlElement* xml_root = xml_conf.RootElement();
+    if (NULL == xml_root)
+    {
+        exit(1);
+        return -1;
+    }
+    
+    TiXmlElement* xml_settings = xml_root->FirstChildElement("logsettings");
+    TiXmlElement* xml_param = xml_settings->FirstChildElement("param");
+    LogModule_l* tail = logconfigs->modules;
+    assert(tail == NULL);
+    for (; xml_param != NULL; xml_param = xml_param->NextSiblingElement() ) 
+    {
+        LogModule_l* module = NULL;
+        TiXmlAttribute* attributeOfParam = xml_param->FirstAttribute();
+        for ( ; attributeOfParam != NULL; attributeOfParam = attributeOfParam->Next() )
+        {
+            const char* name = attributeOfParam->Name();
+            const char* value = attributeOfParam->Value();
+            if(strncmp(name,"logpath", strlen("logpath")) == 0)
+            {
+                int len = strlen(value)+1;
+                logconfigs->logpath = new char[len];
+                snprintf(logconfigs->logpath,len, "%s", value);
+            }
+            else if(strncmp(name,"module", strlen("module")) == 0)
+            {
+                int len = strlen(value)+1;
+                if(module == NULL)
+                {
+                    module = new LogModule_l();
+                    module->module_name = NULL;
+                    module->level = INFO_LOG;
+                    module->next = NULL;
+                }
+                if(module->module_name)
+                {
+                    delete module->module_name;
+                }
+                module->module_name = new char[len];
+                snprintf(module->module_name, len, "%s", value);
+            }
+            else if(strncmp(name,"loglevel", strlen("loglevel")) == 0)
+            {
+                if(module == NULL)
+                {
+                    module = new LogModule_l();
+                    module->module_name = NULL;
+                    module->level = INFO_LOG;
+                    module->next = NULL;
+                }
+                if(strstr(value, "ERROR"))
+                {
+                    module->level = ERROR_LOG;
+                }
+                else if(strstr(value, "WARN"))
+                {
+                    module->level = WARNING_LOG;
+                }
+                else if(strstr(value, "DEBUG"))
+                {
+                    module->level = DEBUG_LOG;
+                }
+            }
+        }
+        if(module)
+        {
+            if(tail == NULL)
+            {
+                tail = module;
+            }
+            else
+            {
+                tail->next = module;
+                tail = module;
+            }
+        }
+    }
+
+    return 0;
+}
+
+void show_sig_handler(int sig)
+{
+    tracelog("SIGNAL", WARNING_LOG, __FILE__, __LINE__, "I got signal %d\n", sig);
+    usleep(200000);
+    tracelog("SIGNAL", WARNING_LOG, __FILE__, __LINE__, "I got signal %d and sleep 200ms\n", sig);
+}
+
+void  processSignal()
+{
+    // Ignore the SIGPIPE signal
+    struct sigaction action_pipe;
+    struct sigaction action_init;
+    struct sigaction action_quit;
+    struct sigaction action_hup;
+    struct sigaction action_ill;
+    struct sigaction action_kill;
+    struct sigaction action_alarm;
+    struct sigaction action_term;
+    struct sigaction action_continue;
+
+    memset(&action_pipe, 0, sizeof(struct sigaction));
+    memset(&action_init, 0, sizeof(struct sigaction));
+    memset(&action_quit, 0, sizeof(struct sigaction));
+    memset(&action_hup, 0, sizeof(struct sigaction));
+    memset(&action_ill, 0, sizeof(struct sigaction));
+    memset(&action_kill, 0, sizeof(struct sigaction));
+    memset(&action_alarm, 0, sizeof(struct sigaction));
+    memset(&action_term, 0, sizeof(struct sigaction));
+    memset(&action_continue, 0, sizeof(struct sigaction));
+
+    action_pipe.sa_handler = SIG_IGN;
+    sigaction(SIGPIPE, &action_pipe, NULL); 
+    
+    action_init.sa_handler = show_sig_handler;
+    sigaction(SIGINT, &action_init, NULL); 
+
+    action_quit.sa_handler = show_sig_handler;
+    sigaction(SIGQUIT, &action_quit, NULL); 
+
+    action_hup.sa_handler = show_sig_handler;
+    sigaction(SIGHUP, &action_hup, NULL); 
+
+    action_ill.sa_handler = show_sig_handler;
+    sigaction(SIGILL, &action_ill, NULL); 
+
+    action_kill.sa_handler = show_sig_handler;
+    sigaction(SIGKILL, &action_kill, NULL); 
+
+    action_alarm.sa_handler = show_sig_handler;
+    sigaction(SIGALRM, &action_alarm, NULL); 
+
+    action_term.sa_handler = show_sig_handler;
+    sigaction(SIGTERM, &action_term, NULL);
+ 
+    action_continue.sa_handler = show_sig_handler;
+    sigaction(SIGCONT, &action_continue, NULL);
+}
+
+static const  char* configurationFile = "./conf/conf.xml";
+void init()
+{
+    LogConfigure log_conf = {.logpath = NULL, .modules = NULL};   
+    parseXMLconfiguration(configurationFile, &log_conf);
+    initLog(log_conf.logpath);
+    delete log_conf.logpath;
+    log_conf.logpath = NULL;
+    LogModule_l* module = log_conf.modules;
+    while(module)
+    {
+        regiterLog(module->module_name, module->level);
+        LogModule_l* tmp = module;
+        module = module->next;
+        delete tmp;
+    }
+    log_conf.modules = NULL;
+
+    processSignal();
+    //initcli(s_configures.cliip, s_configures.cliport); 
+    initTask();
+}
+
+int main(int argc, char* argv[])
+{
+    {
+        struct option longOpts[] = { {"help", 0, 0, 'h'}, {"cwd", 1, 0, 'c'} };
+        char dir[256] = {0};
+        while(1)
+        {
+            int option_index = 0; 
+            int opt = getopt_long(argc, argv, "c:h", longOpts, &option_index);
+            if (opt == -1)
+            {
+                break; 
+            }
+            switch (opt)
+            {
+                case 'c':
+                {
+                    unsigned int i = 0;
+                    unsigned int j = 0;
+                    for (i=0; i<strlen(optarg); i++)
+                    {
+                        if(optarg[i] == '"')
+                        {
+                            continue;
+                        }
+                        else
+                        {
+                            if(j == sizeof(dir))
+                            {
+                                exit(1);
+                            }
+                            dir[j] = optarg[i];
+                            j++;   
+                        }
+                    }
+                    break;
+                }
+                case 'h':
+                default:
+                    printf("sipp --cwp= or \r\n sipp -c  \r\n");
+                    return 0;
+            }
+        }
+        daemonize();
+        {
+            // Change the current working directory to the cwd, allows none root users to run this service. 
+            if (chdir(dir) < 0)
+            {
+                exit(1);
+            }
+        }
+        init();
+    }
+    while(1)
+    {
+        usleep(600);
+    }
+    return 0;
+}
